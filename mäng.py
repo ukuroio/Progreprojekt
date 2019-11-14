2b52c983590eda22476db6ad86ddda88fed5bab3
Uku <you@example.com>
2019-11-14 16:24:23 +0200

    1 kukkuv kast

--- mäng.py
+++ mäng.py
@@ -1,7 +1,35 @@
 import pygame
+import random
 pygame.init()
 
+valge = (255, 255, 255)
+must = (0, 0, 0)
+punane = (255, 0, 0)
+roheline = (0, 255, 0)
+sinine = (0, 0, 255)
+
 pygame.display.set_caption("Mäng")
+
+kõik_vastased = pygame.sprite.Group()
+
+class vastane(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface((30, 30)) # ruudu suurus
+        self.image.fill(punane)
+        self.rect = self.image.get_rect() # teeb ruudu imagest
+        self.rect.y = random.randrange(0, 100) #random koht kus see ruut spawnib
+        self.rect.x = random.randrange(0, 500)
+    def update(self):
+        self.rect.y += 5
+        if self.rect.y > ekraanikõrgus:
+            self.uus_positsioon()
+    def uus_positsioon(self): # saadab ruudu tagasi üles kui see mapist alla kukub
+        self.rect.y = random.randrange(0, 100)
+        self.rect.x = random.randrange(0, 500)
+        
+vast = vastane()
+kõik_vastased.add(vast) # lisab ruudu sprite gruppi
 
 ekraanilaius = 500
 ekraanikõrgus = 500
@@ -9,8 +37,8 @@ ekraanikõrgus = 500
 aken = pygame.display.set_mode((ekraanilaius, ekraanikõrgus))
 
 kiirus = 10
-x=100
-y=100
+x=250
+y=400
 
 run=True
 while run:
@@ -21,18 +49,22 @@ while run:
         if event.type == pygame.QUIT:
             run=False
     
-    if nooled[pygame.K_LEFT]:
+    if nooled[pygame.K_LEFT] and x > kiirus:
         x -=kiirus
-    if nooled[pygame.K_RIGHT]:
+    if nooled[pygame.K_RIGHT] and x < ekraanilaius - 40 - kiirus:
         x += kiirus
-    if nooled[pygame.K_UP]:
+    if nooled[pygame.K_UP] and y > kiirus:
         y -=kiirus
-    if nooled[pygame.K_DOWN]:
+    if nooled[pygame.K_DOWN] and y < ekraanikõrgus- 70 -kiirus:
         y +=kiirus
         
     aken.fill((0, 0, 0))
     pygame.draw.rect(aken,(131, 234, 255), (x, y, 40, 70))
+    
+    kõik_vastased.update()
+    kõik_vastased.draw(aken)#joonistab aknasse Classi
     pygame.display.update()
-
+    pygame.display.flip()
+    
 
 pygame.quit()
