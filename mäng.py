dd838218e3751cc8b9552bdb27ad51e37fb62329
Uku <you@example.com>
2019-12-11 23:07:11 +0200

    player class

--- mäng.py
+++ mäng.py
@@ -13,6 +13,21 @@ pygame.display.set_caption("Mäng")
 clock = pygame.time.Clock()
 
 kõik_vastased = pygame.sprite.Group()
+all_sprites = pygame.sprite.Group()
+
+kiirus = 15
+x=250
+y=450
+
+class mängija(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface((30, 30))
+        self.image.fill(sinine)
+        self.rect = self.image.get_rect()
+        self.rect.y = y
+        self.rect.x = x
+        self.kiirus = 15
 
 class vastane(pygame.sprite.Sprite):
     def __init__(self):
@@ -37,16 +52,15 @@ for i in range(15):
 
     kõik_vastased.add(vast)
 
-
+mängija1 = mängija()
+all_sprites.add(mängija1)
 
 ekraanilaius = 500
 ekraanikõrgus = 500
 
 aken = pygame.display.set_mode((ekraanilaius, ekraanikõrgus))
 
-kiirus = 15
-x=250
-y=450
+
 
 run=True
 while run:
@@ -57,19 +71,18 @@ while run:
         if event.type == pygame.QUIT:
             run=False
     
-    if nooled[pygame.K_LEFT] and x > kiirus:
-        x -=kiirus
-    if nooled[pygame.K_RIGHT] and x < ekraanilaius - 40 - kiirus:
-        x += kiirus
-    if nooled[pygame.K_DOWN] and y < ekraanikõrgus- 70 -kiirus:
-        y +=kiirus
+    if nooled[pygame.K_LEFT] and mängija1.rect.x > mängija1.kiirus:
+        mängija1.rect.x -=mängija1.kiirus
+    if nooled[pygame.K_RIGHT] and mängija1.rect.x < ekraanilaius - 30 - mängija1.kiirus:
+        mängija1.rect.x += mängija1.kiirus
         
     aken.fill((0, 0, 0))
-    
-    pygame.draw.rect(aken,(131, 234, 255), (x, y, 30, 30)) 
+
    
     kõik_vastased.update()
     kõik_vastased.draw(aken)
+    all_sprites.update()
+    all_sprites.draw(aken)
     pygame.display.update()
     clock.tick(27)
     pygame.display.flip()
